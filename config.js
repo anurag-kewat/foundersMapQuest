ecf6dfd23f88dc9098390b45c9944091e5f49714
Anurag Kewat <anurag_kewat@hotmail.com>
2016-07-11 15:22:41 +0530

    created express server using Node

---
+++ config.js
@@ -0,0 +1,11 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(__dirname + '/'));
+
+app.listen(4000);
+console.log('Listening.. on port 5050');
